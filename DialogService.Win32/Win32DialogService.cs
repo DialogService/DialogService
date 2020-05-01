2697aa8e989539ba371a8d7cd2ebe1d886c98067
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-05-01 17:04:03 +0300

    Begin DialogService.Win32 implementation

---
+++ DialogService.Win32/Win32DialogService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DialogService.Win32
+{
+    public class Win32DialogService : IDialogService
+    {
+        public DialogResult Result => throw new NotImplementedException();
+
+        public void Show()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
