c12e37d9787a17f69ec338af7570e4039a768947
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-05-01 17:38:53 +0300

    Refactor DialogService: decouple logic and model

---
+++ DialogService/Dialog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DialogService
+{
+    /// <summary>
+    /// Dialog model
+    /// </summary>
+    public class Dialog
+    {
+        /// <summary>
+        /// Gets or sets dialog's title text
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Creates an empty dialog
+        /// </summary>
+        public Dialog()
+        {
+        }
+
+        /// <summary>
+        /// Creates a dialog with a specified title text
+        /// </summary>
+        /// <param name="title">Title text</param>
+        public Dialog(string title) : this()
+        {
+            Title = title;
+        }
+    }
+}
--- DialogService/IDialogService.cs
+++ DialogService/IDialogService.cs
@@ -1,8 +1,14 @@
 ﻿namespace DialogService
 {
+    /// <summary>
+    /// Defines basic dialog-related functionality
+    /// </summary>
     public interface IDialogService
     {
-        DialogResult Result { get; }
-        void Show();
+        /// <summary>
+        /// Shows a dialog
+        /// </summary>
+        /// <param name="dialog">Dialog model</param>
+        void Show(Dialog dialog);
     }
 }
