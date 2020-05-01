db6e8ded430527d6a53db242180ff3d5a1048a2a
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-05-01 23:23:11 +0300

    Begin Win32 dialog service implementation

--- DialogService.Win32/Win32DialogService.cs
+++ DialogService.Win32/Win32DialogService.cs
@@ -1,5 +1,6 @@
 ﻿using DialogService.Items;
 using System;
+using System.Threading;
 
 namespace DialogService.Win32
 {
@@ -10,7 +11,37 @@ namespace DialogService.Win32
     {
         public void Show(Dialog dialog)
         {
-            throw new NotImplementedException();
+            var thread = new Thread(() => ShowThread(dialog));
+
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.IsBackground = true;
+            thread.Start();
+
+            while (thread.IsAlive)
+                Thread.Sleep(300);
+        }
+
+        private void ShowThread(Dialog dialog)
+        {
+            var mainForm = new MainWindow();
+
+            var converter = new Converter();
+            converter.Converters.Add(new LabelConverter());
+            converter.Converters.Add(new ButtonConverter());
+
+            mainForm.Title = dialog.Title;
+
+            // Converting bottom panel
+            var bottomControls = converter.GetControls(dialog.BottomPanel);
+            foreach (var ctrl in bottomControls)
+                mainForm.BottomPanel.Children.Add(ctrl);
+
+            // Converting main controls
+            var mainControls = converter.GetControls(dialog.Items);
+            foreach (var ctrl in mainControls)
+                mainForm.MainPanel.Children.Add(ctrl);
+
+            mainForm.ShowDialog();
         }
     }
 }
