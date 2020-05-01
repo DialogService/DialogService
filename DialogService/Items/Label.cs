a59917fde312133d443e399bcb5521da65c66bed
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-05-01 20:20:21 +0300

    Label implementation

---
+++ DialogService/Items/Label.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DialogService.Items
+{
+    /// <summary>
+    /// Text label item
+    /// </summary>
+    public class Label : IDialogItem, IContentItem<string>
+    {
+        public object Tag { get; set; }
+        public string Content { get; set; }
+
+        /// <summary>
+        /// Creates a label with specific text
+        /// </summary>
+        /// <param name="content">Text to display</param>
+        public Label(string content = "")
+        {
+            Content = content;
+        }
+    }
+}
