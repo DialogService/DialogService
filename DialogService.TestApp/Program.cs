eecc2bf917f10954e7cca3b9e0d4d35558670d01
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-05-03 02:13:34 +0300

    Good old test projects are back: merging with CrossPlatform.*

---
+++ DialogService.TestApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using DialogService.Items;
+using System;
+
+namespace DialogService.TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DialogService Test Application");
+
+            var dsBuilder = new DialogPlatformBuilder();
+            dsBuilder.AddPlatform<Win32.PlatformImplementation>();
+            dsBuilder.AddPlatform<Linux.PlatformImplementation>();
+            dsBuilder.AddPlatform<MacOS.PlatformImplementation>();
+            var dialogService = dsBuilder.GetService();
+
+            var dialog = new Dialog();
+            dialog.Title = "The Dialog";
+
+            dialog.Items.Add(new Button("Click me"));
+            dialog.Items.Add(new Label("This is the label!"));
+
+            dialog.BottomPanel.Add(new Button("OK"));
+            dialog.BottomPanel.Add(new Button("Cancel"));
+
+            dialogService.Show(dialog);
+
+            Console.WriteLine("Finished");
+        }
+    }
+}
