b09ba50f87155942bb979e9dbd205b8d6e45b0ff
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-05-02 21:35:01 +0300

    Add few platform specification, bump build

--- DialogService/AbstractPlatform.cs
+++ DialogService/AbstractPlatform.cs
@@ -4,8 +4,19 @@ using System.Text;
 
 namespace DialogService
 {
+    /// <summary>
+    /// IDialogService builder for platform
+    /// </summary>
     public abstract class AbstractPlatform
     {
+        /// <summary>
+        /// Gets target platforms
+        /// </summary>
+        public abstract RuntimePlatform Platform { get; }
+        /// <summary>
+        /// Gets IDialogService implementation
+        /// </summary>
+        /// <returns></returns>
         public abstract IDialogService Get();
     }
 }
---
+++ DialogService/RuntimePlatform.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DialogService
+{
+    public enum RuntimePlatform
+    {
+        Unknown,
+        Linux,
+        MacOS,
+        Windows
+    }
+}
