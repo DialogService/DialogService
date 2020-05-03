cc9572e599ec02a30eb44c312b5dd2f83cfc183a
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-05-03 22:33:18 +0300

    Create IEditableItem.cs

---
+++ DialogService/Items/Interfaces/IEditableItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DialogService.Items
+{
+    /// <summary>
+    /// An item that can be modified by user
+    /// </summary>
+    public interface IEditableItem : IDialogItem
+    {
+        /// <summary>
+        /// Gets or sets value if this item can be modified by user
+        /// </summary>
+        bool AllowModification { get; set; }
+    }
+}
