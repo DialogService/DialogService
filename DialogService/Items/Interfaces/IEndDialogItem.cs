5822fc09a908f40871ce48f3d8bf74827c3eefc8
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-05-03 22:33:11 +0300

    Create IEndDialogItem.cs

---
+++ DialogService/Items/Interfaces/IEndDialogItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DialogService.Items
+{
+    /// <summary>
+    /// Defines an <see cref="IDialogItem"/> that ends dialog on user interaction
+    /// </summary>
+    public interface IEndDialogItem : IDialogItem
+    {
+        /// <summary>
+        /// Ends dialog on interaction if <see cref="true"/>
+        /// </summary>
+        bool CloseOnInteract { get; set; }
+    }
+}
