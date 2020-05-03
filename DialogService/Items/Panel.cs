9e8d193533804414616a34b7a5894e2c12673ed4
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-05-03 22:35:09 +0300

    Create Panel.cs

---
+++ DialogService/Items/Panel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DialogService.Items
+{
+    /// <summary>
+    /// Container for <see cref="IDialogItem"/>
+    /// </summary>
+    public class Panel : IDialogItem, IBigContainerItem
+    {
+        public object Tag { get; set; }
+        /// <summary>
+        /// Gets items list
+        /// </summary>
+        public IDialogItemList Items { get; private set; } = new DialogItemList();
+
+        /// <summary>
+        /// Creates an instance of <see cref="Panel"/>
+        /// </summary>
+        public Panel() { }
+
+        /// <summary>
+        /// Creates an instance of <see cref="Panel"/> and adds items
+        /// </summary>
+        /// <param name="items">Items to add</param>
+        public Panel(IEnumerable<IDialogItem> items)
+        {
+            Items.AddRange(items);
+        }
+    }
+}
