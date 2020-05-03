7a3a2d6fb43abaf01812bc5701d3e9ce33f7f83c
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-05-03 22:35:17 +0300

    Create TextBox.cs

---
+++ DialogService/Items/TextBox.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DialogService.Items
+{
+    /// <summary>
+    /// An editable by user text box
+    /// </summary>
+    public class TextBox : IDialogItem, IContainerItem<string>, ITextItem, IEditableItem
+    {
+        /// <summary>
+        /// Gets or sets a tag
+        /// </summary>
+        public object Tag { get; set; }
+        /// <summary>
+        /// Gets or sets text value
+        /// </summary>
+        public string Content { get; set; }
+        /// <summary>
+        /// Gets or sets <see cref="IFont"/>
+        /// </summary>
+        public IFont Font { get; set; }
+        /// <summary>
+        /// Gets or sets value if this <see cref="TextBox"/> can be modified by user
+        /// </summary>
+        public bool AllowModification { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
