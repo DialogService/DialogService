7d3ea82e4ebcbcab6190dfb6f7c8a9fc3275c232
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-05-03 22:33:59 +0300

    IDialogResult

---
+++ DialogService/DialogResult.cs
@@ -0,0 +1,19 @@
+﻿using DialogService.Items;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DialogService
+{
+    public class DialogResult : IDialogResult
+    {
+        public IDialogItem ClosedBy { get; set; }
+
+        public DialogResult() { }
+
+        public DialogResult(IDialogItem closedBy)
+        {
+            ClosedBy = closedBy;
+        }
+    }
+}
---
+++ DialogService/IDialogResult.cs
@@ -0,0 +1,15 @@
+﻿using DialogService.Items;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DialogService
+{
+    public interface IDialogResult
+    {
+        /// <summary>
+        /// Gets or sets <see cref="IDialogItem"/> instance that caused current dialog to close. 
+        /// </summary>
+        IDialogItem ClosedBy { get; set; }
+    }
+}
