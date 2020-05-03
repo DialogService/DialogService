d660ad10acc0160725a2450ab2766603d515db82
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-05-03 22:34:27 +0300

    Create ITextItem.cs

---
+++ DialogService/Items/Interfaces/ITextItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DialogService.Items
+{
+    /// <summary>
+    /// Defines an item that has text with some font settings
+    /// </summary>
+    public interface ITextItem : IDialogItem
+    {
+        IFont Font { get; }
+    }
+}
