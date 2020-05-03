1138c46469819dbf302d0661c30360435d347315
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-05-03 22:33:39 +0300

    Fonts!

---
+++ DialogService/Font.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DialogService
+{
+    public class Font : IFont
+    {
+        /// <summary>
+        /// Font size
+        /// </summary>
+        public int Size { get; set; }
+        /// <summary>
+        /// Font name
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
---
+++ DialogService/IFont.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DialogService
+{
+    /// <summary>
+    /// Defines font settings
+    /// </summary>
+    public interface IFont
+    {
+        /// <summary>
+        /// Font size
+        /// </summary>
+        int Size { get; }
+        /// <summary>
+        /// Font name
+        /// </summary>
+        string Name { get; }
+    }
+}
