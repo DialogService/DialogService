db12b8e90a221a5d615984ea3f65f773d24e783e
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-05-03 22:35:02 +0300

    Update Label.cs

--- DialogService/Items/Label.cs
+++ DialogService/Items/Label.cs
@@ -7,10 +7,21 @@ namespace DialogService.Items
     /// <summary>
     /// Text label item
     /// </summary>
-    public class Label : IDialogItem, IContentItem<string>
+    public class Label : IDialogItem, IContainerItem<string>, IEndDialogItem, ITextItem
     {
         public object Tag { get; set; }
+        /// <summary>
+        /// Gets or sets text value
+        /// </summary>
         public string Content { get; set; }
+        /// <summary>
+        /// Gets or sets value if current dialog must be closed on user interaction with this item
+        /// </summary>
+        public bool CloseOnInteract { get; set; }
+        /// <summary>
+        /// Gets or sets <see cref="IFont"/>
+        /// </summary>
+        public IFont Font { get; set; }
 
         /// <summary>
         /// Creates a label with specific text
