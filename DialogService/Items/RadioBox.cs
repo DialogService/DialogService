87d1c94854b9a49e71955a4f945500fc9aab6673
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-05-03 22:35:14 +0300

    Create RadioBox.cs

---
+++ DialogService/Items/RadioBox.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DialogService.Items
+{
+    public class RadioBox : IDialogItem, IContainerItem, ICheckableItem, IEndDialogItem
+    {
+        public object Tag { get; set; }
+        /// <summary>
+        /// Gets or sets current check state
+        /// </summary>
+        public CheckState CheckState { get; set; } = CheckState.Unchecked;
+        /// <summary>
+        /// Gets or sets value if <see cref="CheckState"/> can be changed by user
+        /// </summary>
+        public bool AllowModification { get; set; } = true;
+        /// <summary>
+        /// Gets or sets value if current dialog must be closed by interaction with this item
+        /// </summary>
+        public bool CloseOnInteract { get; set; }
+        /// <summary>
+        /// Gets or sets content
+        /// </summary>
+        public IDialogItem Content { get; set; }
+    }
+}
