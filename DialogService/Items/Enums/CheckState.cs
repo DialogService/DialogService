0bb66246ddf85ba0740f8bc3f30b4e056cddd4a6
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-05-03 22:33:30 +0300

    ICheckableItem

---
+++ DialogService/Items/Enums/CheckState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DialogService.Items
+{
+    public enum CheckState
+    {
+        Unknown, 
+        Unchecked, 
+        Checked
+    }
+}
---
+++ DialogService/Items/Interfaces/ICheckableItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DialogService.Items
+{
+    /// <summary>
+    /// Defines an item that can be checked by user
+    /// </summary>
+    public interface ICheckableItem : IDialogItem, IEditableItem
+    {
+        /// <summary>
+        /// Gets or sets currect check status.
+        /// </summary>
+        CheckState CheckState { get; set; }
+    }
+}
