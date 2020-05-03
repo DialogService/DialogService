33e1bbfb4e986fdf9845d5dadb7643384d7f51c6
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-05-03 22:34:50 +0300

    Update Button.cs

--- DialogService/Items/Button.cs
+++ DialogService/Items/Button.cs
@@ -4,18 +4,23 @@ using System.Text;
 
 namespace DialogService.Items
 {
-    public class Button : IDialogItem, IContentItem
+    public class Button : IDialogItem, IContainerItem, IEndDialogItem
     {
         public object Tag { get; set; }
-
+        /// <summary>
+        /// Gets or sets content
+        /// </summary>
         public IDialogItem Content { get; set; }
+        /// <summary>
+        /// Get or sets value if this dialog must be closed on user interaction with current instance
+        /// </summary>
+        public bool CloseOnInteract { get; set; } = true;
 
         /// <summary>
         /// Creates an empty button
         /// </summary>
         public Button()
-        {
-        }
+        { }
 
         /// <summary>
         /// Creates a button with specific content
