6a2c8d2a1b85fd4ccb646fa0d67db710f55259ea
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-05-04 21:19:24 +0300

    Revert "No more clickable items"
    This reverts commit d2e0c0beb67ac64495255b67b8ac8a59a1948170.

---
+++ DialogService/Items/Interfaces/IClickableItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DialogService.Items
+{
+    /// <summary>
+    /// Defines an item that can be clicked with mouse buttons
+    /// </summary>
+    public interface IClickableItem : IDialogItem
+    {
+        event EventHandler OnMouseButtonDown;
+        event EventHandler OnMouseButtonUp;
+    }
+}
